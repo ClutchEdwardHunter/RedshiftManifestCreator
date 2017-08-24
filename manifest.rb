e2dc152b4aa07e3e7786585ea2bc3daf63c3531d
ClutchEdwardHunter <edward.hunter@clutch.com>
2017-08-24 10:26:30 -0400

    Initial commit of first refactor

--- manifest.rb
+++ manifest.rb
@@ -1,4 +1,5 @@
 
+
 require 'aws-sdk'
 require 'json'
 
@@ -19,6 +20,7 @@ FLDMASK =  @config["aws_info"]["bucket_info"]["folder_file_mask"]
 MANIFST =  @config["manifest_name"]
 VERBOSE =  @config["verbose_mode"]
 UPLOADM =  @config["upload_manifest"]
+WMANFST =  @config["write_manifest"]
 
 if VERBOSE 
 	puts "RW_AKEY: #{RW_AKEY}\n"
@@ -53,6 +55,7 @@ end
 
 def get_file_keys
 	s3 = Aws::S3::Resource.new
+	file_array = []
 	puts "Targeting bucket (from config): #{TBUCKET}" if VERBOSE
 	bucket=s3.bucket(TBUCKET)
 	puts "Parsing bucket for items matching #{FLDMASK}" if VERBOSE
@@ -60,7 +63,8 @@ def get_file_keys
 		if obj.key.to_s.include? FLDMASK.to_s
 		  puts "Targeting: #{obj.key}" if VERBOSE
   		  manifest_entry(obj.key,"true")
-  		  @files.push(obj.key)
+  		  
+  		  #@files.push(obj.key)
   	    end
     end
    
@@ -73,16 +77,57 @@ end
 def upload_manifest
 	puts "Uploading manifest: #{MANIFST} to S3..." if VERBOSE
 	s3 = Aws::S3::Resource.new
-	obj = s3.bucket(TBUCKET).object(MANIFST)
+	obj = s3.bucket(TBUCKET).object("#{MANIFST}.manifest")
 	obj.upload_file("#{LOCAL_PATH}/#{MANIFST}.manifest")
 end
 
+def get_file_details(s3_obj)
+	details = Hash.new
+	details[:key] = s3_obj.key
+	details[:size] = s3_obj.size
+	details[:owner] = s3_obj.owner
+	details[:last_modified] = s3_obj.last_modified
+	details[:public_url] = s3_obj.public_url
+	details
+end
+
+
+def differential_manifest(json_path_file) #take an input manifest and generate a manifest with the exceptions
+	manifest = Hash.new
+	input_hash = JSON.parse(File.read("#{LOCAL_PATH}/#{json_path_file}"))
+	@manifest = nil #clear out and reset the manifest
+	@manifest = {:entries=>[]}
+	@diff = {:entries=>[]}
+	get_file_keys
+	@manifest[:entries].each do |entry| 
+	  
+	end
+
+
+	return manifest
+end
+
+
+def generate_copy_statement
+#TODO: generate copy sql
+end
+
+
 get_file_keys
-write_manifest_file
+write_manifest_file if WMANFST
+
+=begin
+if ARGV
+	cmd, *arg = ARGV
+	if cmd == '--d' && arg
+		differntial_manifest(arg)
+=end
+
+
+
 if UPLOADM
 	upload_manifest
 else
 	puts "Manifest upload disabled.  Manifest was not uploaded to cloud. \n"
 	puts "Edit upload_manifest key in config file to enable. "
 end
-
